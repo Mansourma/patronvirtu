9a555441c934cebeb210f070c32c0c0f43bdf534
Mohamed Mansour <mohamed.mansour1@usmba.ac.ma>
2023-11-04 19:15:49 +0100

    Update config.php

--- login/config.php
+++ login/config.php
@@ -1,6 +1,6 @@
 <?php
 
-$conn = mysqli_connect("127.0.0.1:3308", "root", "MOHAmed.@98765", "patronvirtu");
+$conn = mysqli_connect("127.0.0.1:3308", "root", "your_password", "patronvirtu");
 
 if (!$conn) {
     echo "Connection Failed";
